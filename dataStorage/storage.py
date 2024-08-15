9fe928d4cf95a816ad6fe3822fd9f87377c745e7
Luna Oley <lunaoley@gmail.com>
2024-08-14 20:59:48 -0400

    SP-05-AddClasses

--- dataStorage/storage.py
+++ dataStorage/storage.py
@@ -1,2 +1,9 @@
 def print_storage():
     print("Hello from storage")
+class dataMaintainer:
+    def __init__(self, argument1, argument2):
+        return
+
+    def print_storage():
+        print("Hello from storage")
+        return
