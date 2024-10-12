8458e30739e41599ba4f201d880c308f6b6a3b2a
Luna Oley <lunaoley@gmail.com>
2024-10-12 19:10:56 -0400

    SP-10-Add log file generation and read/write capability

--- processor.py
+++ processor.py
@@ -6,13 +6,16 @@ from dataStorage.storage import *
 def main():
     print("Hello World")
 
-    storage = dataMaintainer()
+    storage = dataMaintainer("debugLog.log")
     handler = dataHandler()
     reciever = dataReceiver()
 
     handler.print_handling()
     reciever.print_input()
-q    storage.print_storage()
+    storage.print_storage()
+
+    storage.write_to_log_file("I am a bill")
+    storage.read_log_file_to_console("2024-10-12_debugLog.log")
 
 # Using the special variable
 # __name__
